c5a339ba1356837f50b2d8444d6456e96cde8968
Pedro Barros <pedrobars7a@gmail.com>
2024-06-05 19:51:29 -0300

    feat: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.get('/hello', () => {
+  return 'Hello World'
+})
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("Server running on port 3333 🔥");
-});
+  console.log('Server running on port 3333 🔥')
+})
